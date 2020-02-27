50f344ca68a6aba74c1f1a1a9f5ab23c38480cd8
zqa1105 <w_521125@163.com>
2020-02-27 17:58:16 +0800

    静态页面控制层

---
+++ ly-page/src/main/java/com/leyou/page/service/PageService.java
@@ -0,0 +1,54 @@
+package com.leyou.page.service;
+
+import com.leyou.item.pojo.*;
+import com.leyou.page.client.BrandClient;
+import com.leyou.page.client.CategoryClient;
+import com.leyou.page.client.GoodsClient;
+import com.leyou.page.client.SpecificationClient;
+import com.netflix.discovery.converters.Auto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class PageService {
+    private static final Logger logger = LoggerFactory.getLogger(PageService.class);
+
+    @Autowired
+    private GoodsClient goodsClient;
+
+    @Autowired
+    private BrandClient brandClient;
+
+    @Autowired
+    private CategoryClient categoryClient;
+
+    @Autowired
+    private SpecificationClient specClient;
+
+    public Map<String, Object> loadModel(Long spuId) {
+        Map<String,Object> model = new HashMap<>();
+        //查询spu
+        Spu spu = goodsClient.querySpuById(spuId);
+        //查询skus
+        List<Sku> skus = spu.getSkus();
+        //查询详情
+        SpuDetail detail = spu.getSpuDetail();
+        Brand brand = brandClient.queryBrandById(spu.getBrandId());
+        List<Category> categories = categoryClient.queryCateGoryByIds(Arrays.asList(spu.getCid1(), spu.getCid2(), spu.getCid3()));
+        List<SpecGroup> specs = specClient.queryGroupByCid(spu.getCid3());
+        model.put("spu",spu);
+        model.put("skus",skus);
+        model.put("detail",detail);
+        model.put("brand",brand);
+        model.put("categories",categories);
+        model.put("specs",specs);
+        return model;
+    }
+}
